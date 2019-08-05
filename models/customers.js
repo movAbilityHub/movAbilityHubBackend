2ea0be3827752d228252f1d768a11b3a5e3a454b
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-05 20:09:20 +0530

    customer model created

---
+++ models/customers.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const customerSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: Number,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Customer = mongoose.model("Customer", customerSchema);
