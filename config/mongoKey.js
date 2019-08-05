cf71eff5f612df7ac0a7dbff3287f0bd6eb2306e
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-05 19:34:02 +0530

    mongoKey created

---
+++ config/mongoKey.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://uhmijgiezmmzvwar9ghv:wM8dC7A9keQ76q57Rf1J@bkq0t7zuy3jd3ob-mongodb.services.clever-cloud.com:27017/bkq0t7zuy3jd3ob",
+  secretOrKey: "movAbility"
+};
