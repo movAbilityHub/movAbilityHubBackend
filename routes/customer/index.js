f79c602e58d148b9751a265d26f5d2f99f876561
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-05 20:10:55 +0530

    register API updated to save standard Phone Number. Login API updated to retrieve email and phonenumber as part of payload

---
+++ routes/customer/index.js
@@ -0,0 +1,120 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/mongoKey");
+const PhoneNumberValidator = require("awesome-phonenumber");
+
+const passport = require("passport");
+
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
+// Load User model
+const Customer = require("../../models/customers");
+
+// @route POST users/register
+// @desc Register user
+// @access Public
+router.post("/register", (req, res) => {
+  // Form validation
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Customer.findOne({ email: req.body.email }).then(customer => {
+    if (customer) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const phoneNumber = new PhoneNumberValidator(
+        data.phoneNumber,
+        data.countryCode
+      );
+      const standardPhoneNumber = phoneNumber.getNumber("e164");
+
+      const newCustomer = new Customer({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phoneNumber: standardPhoneNumber,
+        password: req.body.password
+      });
+
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newCustomer.password, salt, (err, hash) => {
+          if (err) throw err;
+          newCustomer.password = hash;
+          newCustomer
+            .save()
+            .then(customer => res.status(201).json(customer))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// @route POST users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+  // Form validation
+
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  Customer.findOne({ email }).then(customer => {
+    // Check if user exists
+    if (!customer) {
+      return res.status(404).json({ userNotFound: "User not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, customer.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: customer._id,
+          firstName: customer.firstName,
+          lastName: customer.lastName
+          email: customer.email,
+          phoneNumber: customer.phoneNumber
+        };
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 604800 // 7 days in seconds
+          },
+          (err, token) => {
+            res.status(302).json({
+              token: token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
+
+module.exports = router;
