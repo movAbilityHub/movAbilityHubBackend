745c75befc860d072669297adf9c1640ee02254b
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-07 18:36:14 +0530

    routes for other APIs created

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 //import api file for each operation
 const customer = require("./customer");
+const staffIATA = require("./staffIATA");
+const staffOthers = require("./staffOthers");
+const request = require("./request");
 
 //creating route to the imported location
 router.use("/customer", customer);
+router.use("/staffIATA", staffIATA);
+router.use("/staffOthers", staffOthers);
+router.use("/request", request);
 
 module.exports = router;
