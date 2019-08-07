280b600a8113d51c1fbc03590e766ba54d6f666a
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-07 19:55:51 +0530

    travel agent model updated

--- models/travelAgents.js
+++ models/travelAgents.js
@@ -27,6 +27,10 @@ const travelAgentsSchema = new Schema({
     type: String,
     required: true
   },
+  password: {
+    type: String,
+    required: true
+  },
   date: {
     type: Date,
     default: Date.now
