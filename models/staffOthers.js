c40bfa3a0aebdcfdd0f4606275967c6a08f25c92
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-07 18:20:54 +0530

    Travel agency, Airlines, Airport Agents model created

---
+++ models/staffOthers.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const customerSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  organisationName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: Number,
+    required: true
+  },
+  countryCode: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  code: {
+    type: String,
+    required: true
+  },
+  appliedOn: {
+    type: Date,
+    default: Date.now
+  },
+  approvedOn: {
+    type: Date,
+    required: false
+  }
+});
+
+module.exports = Customer = mongoose.model("Customer", customerSchema);
