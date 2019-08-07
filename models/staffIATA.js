08623017f8d6f13d69cc76b3de63a497f6a14c23
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-07 18:20:27 +0530

    IATA staff model created

---
+++ models/staffIATA.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const customerSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: Number,
+    required: true
+  },
+  countryCode: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  staffID: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Customer = mongoose.model("Customer", customerSchema);
