95bbaeceef4dc1d8daa9c388b978bad451ee9406
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-07 19:20:31 +0530

    routes added for new APIs

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,15 @@ const customer = require("./customer");
 const staffIATA = require("./staffIATA");
 const staffOthers = require("./staffOthers");
 const request = require("./request");
+const careReceiver = require("./careReceiver");
+const travelAgents = require("./travelAgents");
 
 //creating route to the imported location
 router.use("/customer", customer);
 router.use("/staffIATA", staffIATA);
 router.use("/staffOthers", staffOthers);
 router.use("/request", request);
+router.use("/careReceiver", careReceiver);
+router.use("/travelAgents", travelAgents);
 
 module.exports = router;
