9eec274935386da4a63a7ac8274092ce70d0a491
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-15 12:27:24 +0530

    deprication error removed

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const cors = require("cors");
