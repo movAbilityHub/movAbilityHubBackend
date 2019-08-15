82b966cd97ce0bf44af629e45979ca41355a847a
Rohith Prakash <rohithprakash99@gmail.com>
2019-08-15 16:28:58 +0530

    APIs added, store request API updated

--- routes/request/index.js
+++ routes/request/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const passport = require("passport");
 
 const Request = require("../../models/requests");
+const StaffOthers = require("../../models/staffOthers");
 
 const validateStoreRequest = require("../../validation/storeRequest");
 const validateViewOpenRequest = require("../../validation/viewOpenRequest");
@@ -10,6 +11,7 @@ const validateViewClosedRequest = require("../../validation/viewClosedRequest");
 const validateRequestActionByAirline = require("../../validation/requestActionByAirline");
 const validateRequestActionByAirport = require("../../validation/requestActionByAirport");
 const validateCancelRequest = require("../../validation/cancelRequest");
+const validateFetchRequestForStaff = require("../../validation/fetchRequestForStaff");
 
 router.post("/storeRequest", (req, res) => {
   // Form validation
@@ -30,8 +32,8 @@ router.post("/storeRequest", (req, res) => {
       const newRequest = new Request({
         passportNumber: req.body.passportNumber,
         ticketNumber: req.body.ticketNumber,
-        travelDate: req.body.travelDate,
         travelTime: req.body.travelTime,
+        travelDate: req.body.travelDate,
         flightNumber: req.body.flightNumber,
         origin: req.body.origin,
         destination: req.body.destination,
@@ -39,16 +41,23 @@ router.post("/storeRequest", (req, res) => {
         requesterID: req.body.requesterID,
         disability: req.body.disability,
         age: req.body.age,
-        service: req.body.origin,
+        service: req.body.service,
         requestedFor: req.body.requestedFor,
         phoneNumber: req.body.phoneNumber,
         countryCode: req.body.countryCode,
-        transitDestination: req.body.transitDestination
+        transitDestination: req.body.transitDestination,
+        transitDestinationCode: req.body.transitDestinationCode,
+        destinationCode: req.body.destinationCode,
+        originCode: req.body.originCode,
+        airline: req.body.airline,
+        airlineCode: req.body.airlineCode
       });
 
       newRequest
         .save()
-        .then(request => res.status(201))
+        .then(request =>
+          res.status(201).json({ message: "Request raised successfully" })
+        )
         .catch(err => res.status(500).send(err));
     }
   });
@@ -66,7 +75,7 @@ router.post("/viewOpenRequest", (req, res) => {
 
   Request.find({
     requesterID: req.body.requesterID,
-    status: req.body.status
+    closed: false
   })
     .then(request => {
       if (request) {
@@ -88,7 +97,7 @@ router.post("/viewClosedRequest", (req, res) => {
 
   Request.find({
     requesterID: req.body.requesterID,
-    closed: req.body.closed
+    closed: true
   })
     .then(request => {
       if (request) {
@@ -259,6 +268,69 @@ router.post("/closeRequest", (req, res) => {
         id: request._id
       };
       return res.status(200).send({ response: response });
+    }
+  );
+});
+
+router.get("/fetchRegisteredAirport", (req, res) => {
+  StaffOthers.find(
+    { userType: "airport", approved: true },
+    "organisationName code",
+    (err, airports) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).json({ airports: airports });
+    }
+  );
+});
+
+router.get("/fetchRegisteredAirline", (req, res) => {
+  StaffOthers.find(
+    { userType: "airline", approved: true },
+    "organisationName code",
+    (err, airlines) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).json({ airlines: airlines });
+    }
+  );
+});
+
+router.post("/fetchRequestForAirport", (req, res) => {
+  // Form validation
+
+  const { errors, isValid } = validateFetchRequestForStaff(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Request.find(
+    {
+      $or: [{ originCode: req.body.code }, { destinationCode: req.body.code }],
+      closed: req.body.closed
+    },
+    (err, request) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).send({ request: request });
+    }
+  );
+});
+
+router.post("/fetchRequestForAirline", (req, res) => {
+  // Form validation
+
+  const { errors, isValid } = validateFetchRequestForStaff(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Request.find(
+    { airlineCode: req.body.code, closed: req.body.closed },
+    (err, request) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).send({ request: request });
     }
   );
 });
